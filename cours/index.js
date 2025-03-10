1c367463e8da03cd1dafb792e221330638f3fee8
Rayan <sukkarie.rayan@gmail.com>
2025-03-10 22:30:13 +0100

    Ajout du début du projet sur GitHub

---
+++ cours/index.js
@@ -0,0 +1,7 @@
+// ** SELECTEURS **
+// document.querySelector("h4").style.background = "yellow";
+// const baliseHTML = document.querySelector("h4");
+// console.log(baliseHTML);
+// baliseHTML.style.background = "yellow";
+
+//**clickEvent**
